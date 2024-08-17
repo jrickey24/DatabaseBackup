b5ad7e4e4a973364438d4cab37c4383fefa7cbe4
jrickey24 <jrick26@wgu.edu>
2024-08-17 09:25:18 -0500

    git commit -m 'initial commit'

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import pyodbc
+import pandas as pd
+from datetime import datetime
+
+
+def main(db_server, db_name):
+    backup_folder = 'C:/DbBackup'
+    backup_file = db_name + '_' + str(datetime.now().strftime('%Y%m%d_%H%M%S'))
+    backup_path = os.path.join(backup_folder, backup_file) + '.bak'
+    backup_details_file = 'backup_details.csv'
+    conn_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={db_server};DATABASE={db_name};Trusted_Connection=yes"
+
+    try:
+        conn = pyodbc.connect(conn_string)
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute(f"BACKUP DATABASE {db_name} TO DISK='{backup_path}' WITH NOFORMAT, NOINIT, NAME = '{backup_file}',"
+                       f"SKIP, REWIND, NOUNLOAD, STATS = 10")
+        while cursor.nextset():
+            pass
+        conn.close()
+        print("Backup completed successfully.")
+    except pyodbc.Error as e:
+        print("Error:", e)
+
+    backup_details = pd.DataFrame({
+        'database': db_name,
+        'backup_file': backup_file,
+        'backup_time': datetime.now()}, index=[0])
+    backup_df = backup_details
+    backup_df.to_csv(os.path.join(backup_folder, backup_details_file), index=False)
+
+
+if __name__ == '__main__':
+    main()  # pass in db_server & db_name values
